d9ba2b5b8496fb80920272ce17c413ab2b51da8d
Matheus Borges <matheusborges123567@gmail.com>
2026-03-14 02:21:25 -0300

    feat: register student routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from "fastify";
 import { authRoutes } from "./auth";
 import { goalRoutes } from "./goal";
 import { lessonRoutes } from "./lesson";
+import { studentRoutes } from "./student";
 import { userRoutes } from "./user";
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(authRoutes);
   app.register(userRoutes, { prefix: "/users" });
+  app.register(studentRoutes, { prefix: "/students" });
   app.register(goalRoutes, { prefix: "/:student_id/goals" });
   app.register(lessonRoutes, { prefix: "/:student_id/lessons" });
 }
